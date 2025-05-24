82ee578833598deac708d08ffece2d4c5b5b99bc
Mouhieddine Amine <mouhiddine.amine1.com>
2025-05-24 17:42:25 -0400

    Fix map rendering issue with Leaflet in production

--- components/MtlMap.tsx
+++ components/MtlMap.tsx
@@ -1,6 +1,7 @@
 "use client";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
 import { LatLngExpression } from "leaflet";
+import { useEffect } from "react";
 
 interface MapProps {
   center: LatLngExpression;
@@ -9,32 +10,50 @@ interface MapProps {
   height?: string;
 }
 
+function MapFixer() {
+  const map = useMap();
+
+  useEffect(() => {
+    setTimeout(() => {
+      map.invalidateSize(); // Fix layout
+    }, 200); // small delay ensures layout is done
+  }, [map]);
+
+  return null;
+}
+
 export default function Map({
   center,
   zoom,
   markerText,
   height = "400px",
 }: MapProps) {
-
   return (
-    <div style={{ height, width: "100%", maxWidth: "900px", margin: "0 auto", boxShadow: "0 0 10px 3px rgba(230, 184, 33, 0.5)"}}
-    className="rounded-lg overflow-hidden"
+    <div
+      style={{
+        height,
+        width: "100%",
+        maxWidth: "900px",
+        margin: "0 auto",
+        boxShadow: "0 0 10px 3px rgba(230, 184, 33, 0.5)",
+      }}
+      className="rounded-lg overflow-hidden"
     >
-  <MapContainer
-    center={center}
-    zoom={zoom}
-    scrollWheelZoom={false}
-    style={{ height: "100%", width: "100%" }}
-  >
-<TileLayer
-  url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
-  attribution='&copy; <a href="https://carto.com/attributions">CARTO</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-/>
-    <Marker position={center}>
+      <MapContainer
+        center={center}
+        zoom={zoom}
+        scrollWheelZoom={false}
+        style={{ height: "100%", width: "100%" }}
+      >
+        <MapFixer />
+        <TileLayer
+          url={`https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png?api_key=${process.env.NEXT_PUBLIC_STADIA_API_KEY}`}
+          attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>'
+        />
+        <Marker position={center}>
           {markerText && <Popup>{markerText}</Popup>}
         </Marker>
-  </MapContainer>
-</div>
-
+      </MapContainer>
+    </div>
   );
 }
