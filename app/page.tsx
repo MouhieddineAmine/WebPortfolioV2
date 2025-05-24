f63f9fa9e2d8dbe6e6eb57b672ec13e7c206b722
Mouhieddine Amine <mouhiddine.amine1.com>
2025-05-24 17:49:15 -0400

    Fix: Import Leaflet CSS properly in layout.tsx for production

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { Typewriter } from "react-simple-typewriter";
 import Link from 'next/link';
 import dynamic from "next/dynamic";
 
+
 const Map = dynamic(() => import("../components/MtlMap"), { ssr: false });
 
 export default function Home() {
