e92dee6dc46890e753e22ee34ad18731f929dc90
Mouhieddine Amine <mouhiddine.amine1.com>
2025-05-24 17:32:07 -0400

    Update map tile layer to free dark theme without API key

--- components/MtlMap.tsx
+++ components/MtlMap.tsx
@@ -26,11 +26,10 @@ export default function Map({
     scrollWheelZoom={false}
     style={{ height: "100%", width: "100%" }}
   >
-    <TileLayer
-  url={`https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png?api_key=${process.env.NEXT_PUBLIC_STADIA_API_KEY}`}
-  attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>'
+<TileLayer
+  url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+  attribution='&copy; <a href="https://carto.com/attributions">CARTO</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 />
-
     <Marker position={center}>
           {markerText && <Popup>{markerText}</Popup>}
         </Marker>
