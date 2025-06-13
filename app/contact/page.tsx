898e6003d381f67aac6e1321ad0788f90182436b
Mouhieddine Amine <mouhiddine.amine1.com>
2025-06-13 13:41:05 -0400

    fix error

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -57,7 +57,10 @@ const handleSubmit = (e: React.FormEvent) => {
   fetch("/api/contactbackend", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(form),
+    body: JSON.stringify({
+    ...form,
+    recaptchaToken,
+  }),
   })
     .then(res => res.json())
     .then(() => {
