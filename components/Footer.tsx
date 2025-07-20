8dbe64fd106be0c5e052385cca3851e9d28af35f
Mouhieddine Amine <mouhiddine.amine1.com>
2025-07-20 16:03:16 -0400

    responsive footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,12 +8,12 @@ export default function Footer() {
   const [isVerified, setIsVerified] = useState(false);
   return (
     <footer id="footer" className="w-full bg-[#010812] text-gray-400 pt-10 mt-20 border-t border-gray-700 z-50">
-      <div className="max-w-[1500px] mx-auto px-10 grid grid-cols-1 md:grid-cols-4 gap-10 text-sm">
+      <div className="max-w-[1500px] mx-auto px-6 sm:px-8 grid grid-cols-1 md:grid-cols-4 gap-10 text-sm">
 
         {/* Contact Info */}
-        <div className="flex justify-center">
-          <div className="text-left">
-            <h3 className="text-white text-[20px] font-bold mb-4">Contact</h3>
+        <div className="flex justify-center text-center sm:text-left sm:items-start">
+          <div className="text-left mx-auto">
+            <h3 className="text-white text-[20px] font-bold mb-4 text-center">Contact</h3>
 
               <div className="mb-4 text-sm text-gray-300 font-semibold">
     <input
@@ -47,9 +47,9 @@ export default function Footer() {
         </div>
 
         {/* Navigation */}
-        <div className="flex justify-center">
-          <div className="text-left">
-            <h3 className="text-white text-[20px] font-bold mb-4">Navigation</h3>
+        <div className="flex justify-center text-center sm:text-left sm:items-start">
+          <div className="text-lef mx-auto">
+            <h3 className="text-white text-[20px] font-bold mb-4 text-center">Navigation</h3>
             <ul className="space-y-2">
               <li>
                 <Link href="/" className="hover:text-[#E6B821] font-bold">
@@ -71,9 +71,9 @@ export default function Footer() {
         </div>
 
         {/* Socials */}
-        <div className="flex justify-center">
+        <div className="flex justify-center text-center sm:text-left sm:items-start">
           <div className="text-left">
-            <h3 className="text-white text-[20px] font-bold mb-4">Let&apos;s Connect</h3>
+            <h3 className="text-white text-[20px] font-bold mb-4 text-center">Let&apos;s Connect</h3>
             <ul className="space-y-3">
               <li className="flex items-center gap-2 hover:text-[#E6B821]">
                 <FaGithub className="text-lg" />
@@ -102,9 +102,9 @@ export default function Footer() {
         </div>
 
         {/* Languages */}
-        <div className="flex justify-center">
+        <div className="flex justify-center text-center sm:text-left sm:items-start">
           <div className="flex flex-col items-center w-[180px]">
-            <h3 id="language" className="text-white text-[20px] font-bold mb-4">Select Language</h3>
+            <h3 id="language" className="text-white text-[20px] font-bold mb-4 text-center">Select Language</h3>
             <ul className="space-y-2 w-full">
               <li id="btnlanguage-en" className="flex justify-center items-center bg-gray-800 text-white text-sm w-full h-[35px] rounded-md hover:bg-gray-700 transition border-2 border-transparent">
                 <Image src="https://img.icons8.com/color/100/great-britain.png" alt="English" className="w-[35px] object-contain" width={1} height={1} sizes='100vw'/>
